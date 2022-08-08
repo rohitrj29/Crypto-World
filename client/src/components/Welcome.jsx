4ac2f4693f8037b3a6f2b46d753332f0c4aeb3f5
rohitrj29 <f20200906@pilani.bits-pilani.ac.in>
2022-08-08 16:34:43 +0530

    wel icons

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -1,3 +1,7 @@
+import { AiFillPlayCircle } from "react-icons/ai";
+import {SiEthereum} from "react-icons/si";
+import {BsInfoCircle} from 'react-icons/bs';
+import {Loader} from './';
 const Welcome = () => {
     return(
         <h1>Welcome</h1>
