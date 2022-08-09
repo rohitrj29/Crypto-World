5db1c44e837ce5ac16df4b5ba82d7c76b4d85b4c
rohitrj29 <f20200906@pilani.bits-pilani.ac.in>
2022-08-09 23:50:58 +0530

    loader

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -19,8 +19,8 @@ const Welcome = () => {
     const handleSubmit=()=>{}
     return(
         <div className="flex w-full justify-center items-center">
-            <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
-                <div className="flex flex-1 justify-start flex-col md:mr-10">
+            <div className="flex mf:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
+                <div className="flex flex-1 justify-start flex-col mf:mr-10">
                     <h1 className="text-3xl sm:text-5xl text-white text-gradient py-1">Send Crypto<br/>across the world</h1>
                 <p className="text-left mt-5 text-white font-light md:w-9/12 w-11/12 text-base">Explore the Crypto World.Buy and sell cryptocurrencies easily on Krypto </p>
                 <button
