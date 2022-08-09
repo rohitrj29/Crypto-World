91f3ace0fab674b0210ab2f15c5dbdca0d1e5d27
rohitrj29 <f20200906@pilani.bits-pilani.ac.in>
2022-08-09 23:35:37 +0530

    update1

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -60,7 +60,19 @@ const Welcome = () => {
             <Input placeholder="Amount (ETH)" name="amount" type="number" handleChange={handleChange} />
             <Input placeholder="Keyword (Gif)" name="keyword" type="text" handleChange={handleChange} />
             <Input placeholder="Enter Message" name="message" type="text" handleChange={handleChange} />
+            <div className="h-[1px] w-full bg-gray-400 my-2" />
 
+{isLoading
+  ? <Loader />
+  : (
+    <button
+      type="button"
+      onClick={handleSubmit}
+      className="text-white w-full mt-2 border-[1px] p-2 border-[#3d4f7c] hover:bg-[#3d4f7c] rounded-full cursor-pointer"
+    >
+      Send now
+    </button>
+  )}
             
           </div>
           </div>
