a50fae50310ea36d033d667b7116a214691314ac
rohitrj29 <f20200906@pilani.bits-pilani.ac.in>
2022-08-09 23:46:24 +0530

    form

--- client/src/components/Welcome.jsx
+++ client/src/components/Welcome.jsx
@@ -3,9 +3,20 @@ import {SiEthereum} from "react-icons/si";
 import {BsInfoCircle} from 'react-icons/bs';
 import {Loader} from './';
 const companyCommonStyles = "min-h-[70px] sm:px-0 px-2 sm:min-w-[120px] flex justify-center items-center border-[0.5px] border-gray-400 text-sm font-light text-white";
+const Input = ({ placeholder, name, type, value, handleChange }) => (
+    <input
+      placeholder={placeholder}
+      type={type}
+      step="0.0001"
+      value={value}
+      onChange={(e) => handleChange(e, name)}
+      className="my-2 w-full rounded-sm p-2 outline-none bg-transparent text-white border-none text-sm white-glassmorphism"
+    />
+  );
 
 const Welcome = () => {
     const connectWallet=()=>{}
+    const handleSubmit=()=>{}
     return(
         <div className="flex w-full justify-center items-center">
             <div className="flex md:flex-row flex-col items-start justify-between md:p-20 py-12 px-4">
@@ -47,7 +58,7 @@ const Welcome = () => {
               </div>
               <div>
                 <p className="text-white font-light text-sm">
-                  {shortenAddress(currentAccount)}
+                  0xasdah.....hgubbjhkj
                 </p>
                 <p className="text-white font-semibold text-lg mt-1">
                   Ethereum
