b307516ae55d611a1caebd3167437937fac0f8cc
rohitrj29 <f20200906@pilani.bits-pilani.ac.in>
2022-08-11 09:43:47 +0530

    methods

---
+++ smart_contract/contracts/Transactions.sol
@@ -0,0 +1,15 @@
+//SPDX-License-Identifier: UNLICENSED
+pragma solidity ^0.8.0;
+
+contract Transaction {
+    uint256 transactionCounter;
+    event Transfer(address from, address receiver, uint amount , string mesage, uint256 timestamp, string keyword);
+    struct TransferStruct{
+     address sender;
+     address receiver;
+     uint amount;
+     string message;
+     uint256 timestamp;
+     string keyword;
+    }
+}
