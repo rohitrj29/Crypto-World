00da33ebe372eb448a9801ce4e5fd84f8b6be930
rohitrj29 <f20200906@pilani.bits-pilani.ac.in>
2022-08-11 10:03:06 +0530

    transaction

--- smart_contract/contracts/Transactions.sol
+++ smart_contract/contracts/Transactions.sol
@@ -2,7 +2,7 @@
 pragma solidity ^0.8.0;
 
 contract Transaction {
-    uint256 transactionCounter;
+    uint256 transactionCount;
     event Transfer(address from, address receiver, uint amount , string mesage, uint256 timestamp, string keyword);
     struct TransferStruct{
      address sender;
@@ -11,5 +11,17 @@ contract Transaction {
      string message;
      uint256 timestamp;
      string keyword;
+    }
+    TransferStruct[] transactions;
+    function addToBlockchain(address payable receiver, uint amount ,string memory message,string memory keyword) public{
+        transactionCount+=1;
+        transactions.push(TransferStruct(msg.sender,receiver,amount,message,block.timestamp,keyword));
+        emit Transfer(msg.sender,receiver,amount,message,block.timestamp,keyword);
+    }
+    function getTransaction() public view returns(TransferStruct[] memory){
+        
+    }
+    function getTransactionCount() public view returns(uint256){
+      return transactionCount;
     }
 }
